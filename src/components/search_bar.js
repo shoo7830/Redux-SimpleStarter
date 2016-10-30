39d7e88ed24851e83038cf66f2c639f27f87b3c7
KimIn Suk <shoo7830@gmail.com>
2016-10-31 00:30:06 +0900

    chapter01 끝

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,26 @@
 /**
  * Created by shoo7830 on 2016. 10. 29..
  */
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+
+        );
+    }
+}
 
 export default SearchBar;
