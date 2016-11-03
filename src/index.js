c0bbe8b9fc7faacfe60ca623c811d4a806a81bf3
Aria Kim <shoo7830@gmail.com>
2016-11-03 17:54:36 +0900

    chapter02 에서 26 Video List Items까지 학습

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyAD1cD-4WLSTqCB96cZa3nfy_bkx4x0bOc';
 
-const API_KEY = 'AIzaSyDYPoq_IckqRwDKfxdrP9HOSvdOaSq0aI4';
 
-const App = () =>  {
-	return (
-	<div>
-		<SearchBar />
-	</div>
-	);
+
+class App extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = { videos: [] };
+
+	  YTSearch({key: API_KEY, term: '트와이스TT'}, (videos) => {
+	  	this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+		<div>
+			<SearchBar />
+			<VideoList videos={this.state.videos} />
+		</div>
+		);
+	}	
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
