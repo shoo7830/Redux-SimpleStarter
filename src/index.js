9440f46b675c260fd4307ef7a2ace0a9461485a2
Aria Kim <shoo7830@gmail.com>
2016-11-08 14:37:49 +0900

    Modern React with Redux Section2 chapter29까지 학습
    비디오 상세 영역, 출력된 비디오 리스트에서 클릭하면 상세 영역이 변경되는 부분까지 작업

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAD1cD-4WLSTqCB96cZa3nfy_bkx4x0bOc';
 
 
@@ -11,10 +12,16 @@ class App extends Component {
 	constructor(props) {
 	  super(props);
 	
-	  this.state = { videos: [] };
+	  this.state = { 
+	  	videos: [],
+	  	selectVideo: null
+	  };
 
-	  YTSearch({key: API_KEY, term: '트와이스TT'}, (videos) => {
-	  	this.setState({ videos });
+	  YTSearch({key: API_KEY, term: 'twice TT'}, (videos) => {
+	  	this.setState({ 
+	  		videos: videos,
+	  		selectedVideo: videos[0] 
+	  	});
 		});
 	}
 
@@ -22,7 +29,10 @@ class App extends Component {
 		return (
 		<div>
 			<SearchBar />
-			<VideoList videos={this.state.videos} />
+			<VideoDetail video={this.state.selectedVideo} />
+			<VideoList 
+				onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+				videos={this.state.videos} />
 		</div>
 		);
 	}	
